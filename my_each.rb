6b6d63db27225e6964a821dc55f0ff97114f6055
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-05 17:33:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
-def my_each(array)
+def my_each(collection)
   i = 0
- while i < array.length
- yield (array[i])
+ while i < collection.length
+ yield (collection[i])
   i = i + 1
  end
- array
+ collection
 end
 
 collection = [1, 2, 3, 4]
